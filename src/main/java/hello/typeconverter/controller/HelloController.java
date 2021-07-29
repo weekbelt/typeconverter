22bf17fab164794b601b4edeab32893a5f038737
weekbelt <vfrvfr4207@gmail.com>
2021-07-29 23:18:51 +0900

    스프링에 Converter 적용하기

--- src/main/java/hello/typeconverter/controller/HelloController.java
+++ src/main/java/hello/typeconverter/controller/HelloController.java
@@ -1,5 +1,6 @@
 package hello.typeconverter.controller;
 
+import hello.typeconverter.type.IpPort;
 import javax.servlet.http.HttpServletRequest;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
@@ -20,5 +21,11 @@ public class HelloController {
     public String helloV2(@RequestParam Integer data) {
         System.out.println("data = " + data);
         return "ok";
+    }
+
+    @GetMapping("/ip-port")
+    public String ipPort(@RequestParam IpPort ipPort) {
+        System.out.println("ipPort IP = " + ipPort.getIp());
+        System.out.println("ipPort PORT = " + ipPort.getPort());
     }
 }
